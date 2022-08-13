daa04536ea6f348c33199c91ea83a8741be94f29
ale9516 <juan.gutierrez19958@gmail.com>
2022-08-13 12:52:07 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,18 @@
+export const tasks = [ //cuando es export default no ponemos {}
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi tercera tarea",
+  },
+];
+//lo que quiero hacer es utilizar los 3 datos, tengo que guardarlo en un estado
